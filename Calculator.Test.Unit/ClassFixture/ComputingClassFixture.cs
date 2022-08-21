13015a5747da9d0eff47c613e797412c81a1e277
soheil aghaei <soheil.sa7813@gmail.com>
2022-08-21 18:16:01 +0430

    Init Unit Test

---
+++ Calculator.Test.Unit/ClassFixture/ComputingClassFixture.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculator.Test.Unit.ClassFixture;
+
+public class ComputingClassFixture:IDisposable
+{
+    public Computing computing;
+
+    public ComputingClassFixture()
+    {
+        computing = new();
+    }
+
+    public void Dispose()
+    {
+        //
+    }
+}
--- Calculator.Test.Unit/ComputingTests.cs
+++ Calculator.Test.Unit/ComputingTests.cs
@@ -1,23 +1,66 @@
-﻿namespace Calculator.Test.Unit;
+﻿using Calculator.Test.Unit.ClassFixture;
+using FluentAssertions;
 
-public class ComputingTests
+namespace Calculator.Test.Unit;
+
+public class ComputingTests:IClassFixture<ComputingClassFixture>
 {
+    private Computing computing;
+
+    public ComputingTests(ComputingClassFixture c)
+    {
+        computing =c.computing;
+    }
+
+
     [Fact]
     public void OddOrEven_Should_Return_Odd_When_Input_Is_OddValue()
     {
-        var computing = new Computing();
+        //act
         var result=computing.OddOrEven(11);
 
-        Assert.Equal("Odd",result);
+        //Assert.Equal("Odd",result);
+
+        //assserts
+        result.Should().Be("Odd");
     }
 
     [Theory]
     [InlineData(10)]
     public void OddOrEven_Should_Return_Even_When_Input_Is_EvenValue(int value)
     {
-        var computing = new Computing();
         var result = computing.OddOrEven(value);
 
-        Assert.Equal("Even", result);
+        //Assert.Equal("Even", result);   ////bedoone estefade az fluent assertion 
+        result.Should().Be("Even");      //ba estefade az fluent assertion 
+    }
+
+    [Fact]
+    public void CalculateAge_Should_Calculate_Age_When_CurrentYear_And_BirthDate_Is_ValidData()
+    {
+        var result = computing.CalculateAge(1380, 1400);
+        result.Should().Be(20);
+    }
+    [Fact]
+    public void CalculateAge_Should_Return_Zero_When_BirthDate_LessThan_Zero()
+    {
+        var result = computing.CalculateAge(-1, 1400);
+        result.Should().Be(0);
+    }
+
+    [Fact]
+    public void CalculateAge_Should_Throw_ArgumentException_When_BirthDate_OR_CurrentYear_Is_Zero()
+    {
+        var result = new Action(() =>
+        {
+            computing.CalculateAge(0, 1400);
+        });
+        result.Should().Throw<ArgumentException>();
+    }
+
+
+    public void Dispose()
+    {
+        
     }
 }
