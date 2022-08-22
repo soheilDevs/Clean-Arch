09cb40ca0012a6a82957e3e462798daec6d0a31e
soheil aghaei <soheil.sa7813@gmail.com>
2022-08-23 04:05:47 +0430

    Add Product Command Validator

--- Application/Products/Create/CreateProductCommandHandler.cs
+++ Application/Products/Create/CreateProductCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.ProductAgg;
 using Domain.Products;
 using Domain.Shared;
+using Domain.Shared.Exceptions;
 using MediatR;
 
 namespace Application.Products.Create;
@@ -15,6 +16,11 @@ public class CreateProductCommandHandler:IRequestHandler<CreateProductCommand>
     }
     public async Task<Unit> Handle(CreateProductCommand request, CancellationToken cancellationToken)
     {
+        var validator = new CreateProductCommandValidator();
+        var checker = validator.Validate(request);
+        if (!checker.IsValid)
+            throw new InvalidDomainDataException(checker.Errors[0].ToString());
+
         var product = new Product(request.Title, Money.FromToman(request.Price),request.Description);
         _repository.Add(product);
        await _repository.SaveChanges();
---
+++ Application/Products/Create/CreateProductCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Application.Products.Create;
+
+public class CreateProductCommandValidator:AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(r => r.Description)
+            .NotEmpty().WithMessage("توضیحات را وارد نمایید")
+            .MinimumLength(10).WithMessage("توضیحات باید بیشتر از 10 کاراکتر باشد");
+
+        RuleFor(r => r.Price)
+            .GreaterThanOrEqualTo(0).WithMessage("مبلغ وارد شده نا معتبر است");
+    }
+}
